17d967c75241e30f6cb1d47d6bb5041f180cafd0
Antoine T-S <anto291100@gmail.com>
2021-02-26 20:51:43 -0500

    push pour bakathomas

---
+++ Assets/Scripts/Events/HandKnife.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandKnife : MonoBehaviour
+{
+    public float shakeAmplitude;
+    public float shakeSpeed;
+    private Vector2 target;
+    private Vector2 shake;
+    private FollowMouse fm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(shakeAmplitude == 0)
+        {
+            shakeAmplitude = 1;
+        }
+        if(shakeSpeed == 0)
+        {
+            shakeSpeed = 1;
+        }
+        fm = GetComponent<FollowMouse>();
+        shake = new Vector2(0, 0);
+        SetTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shake = Vector2.MoveTowards(shake, target, shakeSpeed * Time.deltaTime);
+        fm.xOffset = shake.x;
+        fm.yOffset = shake.y;
+        if(Vector2.Distance(shake,target) < 0.1f)
+        {
+            SetTarget();
+        }
+
+
+
+    }
+
+
+    void SetTarget()
+    {
+        target = new Vector2(Random.Range(-shakeAmplitude, shakeAmplitude), Random.Range(-shakeAmplitude, shakeAmplitude));
+    }
+}
