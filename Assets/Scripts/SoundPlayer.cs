17ba4f7cf67dc20c46cb63efc028c107bbe0a30b
Memoh <CraftMan3512@gmail.com>
2021-02-27 17:11:29 -0500

    Ready for merge with amttys

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -4,22 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public enum Songs
-{
-    
-    ADRENALINE,
-    GameplaySong
-    
-}
-
-public enum SoundEffects
-{
-    
-    MenuButtonPress,
-    JoinGame,
-    
-}
-
 
 public class SoundPlayer : MonoBehaviour
 {
@@ -28,9 +12,6 @@ public class SoundPlayer : MonoBehaviour
     
     private AudioSource source;
 
-    static Dictionary<Songs, AudioClip> songs;
-    static Dictionary<SoundEffects, AudioClip> effects;
-    
     [Range(0,1)]
     public float globalVolumeSet;
     float globalVolume; // global volume multiplier
@@ -43,60 +24,16 @@ public class SoundPlayer : MonoBehaviour
         if (instance is null) instance = this;
         else Destroy(gameObject);
 
-        InitializeSounds();
-
         source = GetComponent<AudioSource>();
         globalVolume = globalVolumeSet;
         
     }
 
-    public void SetMusic(Songs song, bool startPlaying = true)
-    {
-
-        if (songs.ContainsKey(song))
-        {
-
-            if (source.isPlaying) {source.Stop();}
-            source.clip = songs[song];
-            if (startPlaying) {source.Play();}
-
-        }
-
-    }
-
-    public void PlaySFX(SoundEffects sfx, float vol = 1f)
-    {
-
-        if (effects.ContainsKey(sfx))
-        {
-            
-            source.PlayOneShot(effects[sfx],2*globalVolume*vol);   
-            
-        }
-
-    }
-
-    void InitializeSounds()
+    public void PlaySFX(AudioClip sfx, float vol = 1f)
     {
         
-        //put songs in this list
-        songs = new Dictionary<Songs, AudioClip>()
-        {
-            {Songs.ADRENALINE, Resources.Load<AudioClip>("Sound/Music/ADRENALINE")},
-            {Songs.GameplaySong, Resources.Load<AudioClip>("Music/Forest")},
-            //...
-        };
+        source.PlayOneShot(sfx,2*globalVolume*vol);
 
-        //put sfx in this list
-        effects = new Dictionary<SoundEffects, AudioClip>()
-        {
-
-            {SoundEffects.MenuButtonPress, Resources.Load<AudioClip>("Music/Forest")},
-            {SoundEffects.JoinGame, Resources.Load<AudioClip>("Music/Forest")},
-            //...
-
-        };
-        
     }
-    
+
 }
