50502f8d91953d0d4cc7a5ebef17f7874d702b29
Antoine T-S <anto291100@gmail.com>
2021-02-28 11:01:13 -0500

    CONTOUUUUUR

---
+++ Assets/Scripts/Contour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contour : MonoBehaviour
+{
+
+    CanvasGroup cg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cg = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.Sanity < 70)
+        {
+            cg.alpha = 1 - GameManager.Sanity / 100f;
+        }
+        else
+        {
+            cg.alpha = 0;
+        }
+    }
+}
